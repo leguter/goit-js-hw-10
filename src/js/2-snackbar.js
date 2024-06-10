0ecdc963e3f630c5b44dcb67f36f93b17d75ddfe
leguter <kolalevcenko80@gmail.com>
2024-06-09 17:41:55 -1200

    6

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,12 @@
+
+
+
+const refs = {
+    inputDelay: document.querySelector('.input-delay'),
+    inputFulf: document.querySelector('.input-fulf'),
+    inputReje: document.querySelector('.input-reje'),
+    btnEl: document.querySelector('.btn-ntf'),
+};
+refs.btnEl.addEventListener('click', () => {
+    
+})
