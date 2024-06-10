3e9a7bc3ff3fa291d703ab477b0fc7cca5c592df
leguter <kolalevcenko80@gmail.com>
2024-06-10 11:23:12 -1200

    аа

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -2,11 +2,29 @@
 
 
 const refs = {
+    formEl: document.querySelector('.form'),
     inputDelay: document.querySelector('.input-delay'),
     inputFulf: document.querySelector('.input-fulf'),
     inputReje: document.querySelector('.input-reje'),
     btnEl: document.querySelector('.btn-ntf'),
 };
-refs.btnEl.addEventListener('click', () => {
+refs.formEl.addEventListener('submit', (e) => {
+    setTimeout(() => {
+        createPromise(refs.inputDelay.value, refs.inputFulf.value || refs.inputReje.value)
+            .then(suss => console.log(suss))
+            .catch(error => console.error(error));
+    }, refs.inputDelay.value);
+     e.preventDefault();
+});
+function createPromise(delay, input) {
+    if (input === refs.inputFulf.value) {
+        return Promise.resolve(`✅ Fulfilled promise in ${ delay }ms`);
+    } else if (input === refs.inputReje.value) {
+        return Promise.reject(`❌ Rejected promise in ${delay}ms`);
+    }
+}
+// createPromise(1000, refs.inputFulf)
+//   .then(suss => console.log(suss))
+//   .catch(error => console.error(error));
     
-})
+     
