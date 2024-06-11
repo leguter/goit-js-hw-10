b3e544f1f219652aecccde7ef7bc0f13774c551b
leguter <kolalevcenko80@gmail.com>
2024-06-10 22:28:51 -1200

    done

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,4 +1,7 @@
 
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import closeIcon from '../img/close-icon.svg'
 
 
 const refs = {
@@ -7,22 +10,52 @@ const refs = {
     inputFulf: document.querySelector('.input-fulf'),
     inputReje: document.querySelector('.input-reje'),
     btnEl: document.querySelector('.btn-ntf'),
+    radioChecked: document.querySelectorAll('input:checked[type="radio"]')
 };
-refs.formEl.addEventListener('submit', (e) => {
-    setTimeout(() => {
-        createPromise(refs.inputDelay.value, refs.inputFulf.value || refs.inputReje.value)
-            .then(suss => console.log(suss))
-            .catch(error => console.error(error));
-    }, refs.inputDelay.value);
-     e.preventDefault();
+
+
+refs.formEl.addEventListener('submit', createPromise);
+let promise;
+function createPromise(e) {
+    e.preventDefault();
+    const delay = e.target.delay.value;
+    const form = e.target.state.value;
+    
+     promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (form === 'fulfilled') {
+                resolve(delay);
+            } else {
+                reject(delay);
+            }
+        }, delay);
+     });  
+    promise
+        .then(delay => {
+            iziToast.success({
+                message: `✅ Fulfilled promise in ${delay}ms`,
+                backgroundColor: '#59a10d',
+                messageSize: '16px',
+                messageColor: '#fff',
+                position: "topRight",
+                messageLineHeight: '1.5',
 });
-function createPromise(delay, input) {
-    if (input === refs.inputFulf.value) {
-        return Promise.resolve(`✅ Fulfilled promise in ${ delay }ms`);
-    } else if (input === refs.inputReje.value) {
-        return Promise.reject(`❌ Rejected promise in ${delay}ms`);
-    }
-}
+        })
+        .catch(delay => {
+            console.log();
+            iziToast.error({
+                message: `❌ Rejected promise in ${delay}ms`,
+                backgroundColor: '#ef4040',
+                messageLineHeight: '1.5',
+                messageSize: '16px',
+                messageColor: '#fff',
+                position: "topRight",
+                iconUrl: closeIcon,
+});
+        });
+
+};
+
 // createPromise(1000, refs.inputFulf)
 //   .then(suss => console.log(suss))
 //   .catch(error => console.error(error));
